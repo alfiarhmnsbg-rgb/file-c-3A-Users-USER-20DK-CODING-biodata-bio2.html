bf2c429e13216dd7ac2bbbfdba33135add84aef7
alfiarhmnsbg-rgb <alfiarhmn.sbg@gmail.com>
2026-09-20 22:01:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+```javascript
+function tampilkanPesan() {
+
+    const pesan = document.getElementById("pesan");
+
+    pesan.innerHTML =
+        "Senang bertemu dengan Anda! Terima kasih sudah mengunjungi website saya 😊";
+}
+```
